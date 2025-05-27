eaa70ffac6932692f096115919733dc3baedc19d
Meshach Arinze <arinzemeshach123@gmail.com>
2025-05-27 19:34:40 +0100

    refactor: replace auth0 auth with clerk and update the route for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,25 @@
-import { NextResponse, NextRequest } from "next/server";
-import { auth0 } from "./lib/auth0";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export async function middleware(request: NextRequest) {
-  const authRes = await auth0.middleware(request);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/dashboard(.*)",
+  "/sign-in",
+  "/sign-up",
+  "/api/webhooks/clerk",
+  "/api/trpc/[trpc]",
+]);
 
-  // authentication routes — let the middleware handle it
-  if (request.nextUrl.pathname.startsWith("/auth")) {
-    return authRes;
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
-
-  // public routes — no need to check for session
-  if (request.nextUrl.pathname === "/") {
-    return authRes;
-  }
-
-  const { origin } = new URL(request.url);
-  const session = await auth0.getSession();
-
-  // user does not have a session — redirect to login
-  if (!session) {
-    return NextResponse.redirect(`${origin}/auth/shows`);
-  }
-
-  return authRes;
-}
+});
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     * - api (API routes)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|api).*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 };
