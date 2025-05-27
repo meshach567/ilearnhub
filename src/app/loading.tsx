5355df29f0c54ce0660154af085ecad5bb74917e
Meshach Arinze <arinzemeshach123@gmail.com>
2025-05-27 13:45:30 +0100

    feat: remove the loading file from the codebase

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Loading from "../components/ui/Loading";
-
-export default function LoadingPage() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <Loading size={64} color="#6366f1" className="scale-150" />
-    </div>
-  );
-}
